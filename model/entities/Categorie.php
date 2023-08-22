a49efd642035769a1da89cf06f24efa1cbcaaab9
cem-sucu <cems.cems.17@gmail.com>
2023-08-22 15:32:54 +0200

    creation entitie Categorie

---
+++ model/entities/Categorie.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Model\Entities;
+
+use App\Entity;
+
+final class Categorie extends Entity{
+
+    private $id;
+    private $categorie;
+
+    public function __construct($data)
+    {
+        $this->hydrate($data);
+    }
+    
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */ 
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of categorie
+     */ 
+    public function getCategorie()
+    {
+        return $this->categorie;
+    }
+
+    /**
+     * Set the value of categorie
+     *
+     * @return  self
+     */ 
+    public function setCategorie($categorie)
+    {
+        $this->categorie = $categorie;
+
+        return $this;
+    }
+}
+
+
+?>
