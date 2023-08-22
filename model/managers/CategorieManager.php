de1f5b56904c6d7911f50738117260357ce8e1b2
cem-sucu <cems.cems.17@gmail.com>
2023-08-22 16:35:30 +0200

    creation des managers

---
+++ model/managers/CategorieManager.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Model\Managers;
+
+use App\Manager;
+use App\DAO;
+use Model\Managers\CategorieManager;
+
+class CategorieManager extends Manager{
+
+    protected $className = "Model\Entities\Manager";
+    protected $tableName = "categorie";
+
+    public function __construct()
+    {
+        parent::connect();
+    }
+}
+
+
+?>
