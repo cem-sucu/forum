765d0d233cf6a1c1255ca5d8b3e7598f81a09aea
cem-sucu <cems.cems.17@gmail.com>
2023-08-30 10:03:18 +0200

    créatipon et mise en place du form

--- view/security/register.php
+++ view/security/register.php
@@ -1,1 +1,19 @@
-<h1>S'enregistrer</h1>
+<h1>S'inscrire</h1>
+
+<!-- comme le live code de mickael hash.mp4 mais adapter a ma bdd et mes valeurs -->
+<form action="" method="POST">
+    <label for="pseudo">Pseudo</label>
+    <input type="text" name="pseudonyme" id="pseudonyme" required>
+
+    <label for="email">Ermail</label>
+    <input type="email" name="email" id="email" required>
+
+    <label for="password">mot de passe</label>
+    <input type="password" name="motsDePasse" id="motsDePasse" required>
+
+    <label>confirmation de votre mot de passe</label>
+    <input type="password" name="motsDePasseConfirmation" id="motsDePasseConfirmation" required>
+
+    <input type="submit" value="Ajouter">
+
+</form>
