59b0e5f2820893f06ce05e58415b6b74179b5440
cem-sucu <cems.cems.17@gmail.com>
2023-08-30 09:15:33 +0200

    dans SecurityController, comme pour forum et home on copie colle le namespace et les use App

---
+++ controller/SecurityController.php
@@ -0,0 +1,11 @@
+<?php 
+// comme pour forum et Home je vais copier le namespace et les use dont j'aurzis besoin
+
+namespace Controller;
+
+use App\Session;
+use App\AbstractController;
+use App\ControllerInterface;
+
+
+?>
