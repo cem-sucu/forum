ee359f3ee5d1e136ab19a3b39d2427fafdadd32a
cem-sucu <cems.cems.17@gmail.com>
2023-08-30 11:29:22 +0200

    on cree notre function inscription et on prepare nos variable pour pouvoir stocké dedans les donnée saisie

--- controller/SecurityController.php
+++ controller/SecurityController.php
@@ -10,7 +10,27 @@ use Model\Entities\Categorie;
 use Model\Managers\SujetManager;
 use Model\Managers\MessageManager;
 use Model\Managers\CategorieManager;
+
 use Model\Managers\UtilisateurManager;
+
+// pour  gérer les actions associées à la page d'accueil du site
+class HomeController extends AbstractController implements ControllerInterface{
+    public function inscription(){
+        // je creer une insatnce de la classe UtilisateurManager
+        $UtilisateurManager = new UtilisateurManager();
+
+        //si le formulaire est valide est soumis 'submit' pour la vérification
+        if (isset($_Post['submit'])){
+            // on déclare les variable et le filter_input en associant aux bonne valeur du form de register.php
+            $pseudonyme = filter_input(INPUT_POST, "pseudonyme", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            $motsDePasse = filter_input(INPUT_POST, "motsDePasse", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            $motsDePasseConfirmation =filter_input(INPUT_POST, "motsDePasseConfirmation", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            $email =filter_input(INPUT_POST, "email", FILTER_SANITIZE_FULL_SPECIAL_CHARS, FILTER_VALIDATE_EMAIL);
+            $role =filter_input(INPUT_POST, "role", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+        }
+    }
+}
 
 
 ?>
